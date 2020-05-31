8538c1828ac0e6086c7994361592add678df2fa4
d4rkFox <maks.petri4enko@yandex.ru>
2020-05-31 14:40:00 +0700

    added adaptiv home

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,14 @@ $(function () {
     $('.weekly__wrapper').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    arrows: false,
+                },
+            },
+        ],
     })
 
     $(".weekly__star").rateYo({
@@ -32,16 +40,35 @@ $(function () {
         slidesToShow: 3,
         slidesToScroll: 3,
         variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    variableWidth: false,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    variableWidth: false,
+                },
+            },
+        ],
     });
 
-  
+
 
     $('.product__filter-btn').on('click', function () {
         $('.product__dropdown-list')
-        .slideToggle({
-            duration: 200,
-            easing: "linear",
-        })
+            .slideToggle({
+                duration: 200,
+                easing: "linear",
+            })
     });
 
 
@@ -56,15 +83,15 @@ $(function () {
         prefix: "$"
     });
 
-    $(".header__btn").on ('click',function(){
+    $(".header__btn").on('click', function () {
         $('.header__box').toggleClass('header__box--active')
         $('.header__logo-link').toggleClass('header__logo-link--active')
     });
 
-    $(".menu__btn-wrapper").on('click', function(){
+    $(".menu__btn-wrapper").on('click', function () {
         $(".menu__list").slideToggle()
     });
-    
+
     var mixer = mixitup('.product__inner-items');
 });
 
